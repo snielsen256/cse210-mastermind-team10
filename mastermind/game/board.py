aee5fe8bb3e008f42ed9587e4164a812c55db29a
JWilderman <kaodremora@outlook.com>
2021-10-21 15:33:58 -0400

    started board functions

---
+++ mastermind/game/board.py
@@ -0,0 +1,18 @@
+import random
+
+class Board:
+
+    def __init__(self):
+        ""
+
+    def _prepare(self):
+        ""
+
+    def board_string(self):
+        ""
+
+    def check_guess(self):
+        ""
+
+    def apply_guess(self):
+        ""
